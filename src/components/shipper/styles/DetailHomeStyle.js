78734e8df0d0b9bec6993a53ed458cf00c4ae245
New_App <bason1607@gmail.com>
2024-03-07 21:28:34 +0700

    9h28,0703

---
+++ src/components/shipper/styles/DetailHomeStyle.js
@@ -0,0 +1,5 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+export const DetailHomeStyle = StyleSheet.create({});
---
+++ src/http/AxiosInstance.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AxiosInstance = () => {
+  return (
+    <View>
+      <Text>AxiosInstance</Text>
+    </View>
+  )
+}
+
+export default AxiosInstance
