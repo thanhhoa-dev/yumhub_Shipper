419c8e07a5809f31bdb1f49494cfe6131d0d78e6
New_App <bason1607@gmail.com>
2024-04-15 10:21:52 +0700

    ''

---
+++ src/components/shipper/screen/NE.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const NE = () => {
+  return (
+    <View>
+      <Text>NE</Text>
+    </View>
+  )
+}
+
+export default NE
+
+const styles = StyleSheet.create({})
