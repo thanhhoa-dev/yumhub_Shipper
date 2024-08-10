a7dbeb9a8e7630d45fb007b67f5c71a0ee22b9d7
thanhhoadev <doanthanhhoadev@gmail.com>
2024-08-10 09:56:46 +0700

    fix design chat

--- src/components/shipper/screen/ChatWithCustomer.js
+++ src/components/shipper/screen/ChatWithCustomer.js
@@ -2,23 +2,23 @@ import {
     View, Text, TouchableWithoutFeedback,
     FlatList, TextInput,
     TouchableOpacity,
-    Image, Dimensions, 
+    Image, Modal, ScrollView, Dimensions
 } from 'react-native'
 import React, { useContext, useEffect, useState, useCallback, useRef } from 'react'
 import { UserContext } from '../../user/UserContext'
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { styles } from '../styles/ChatWithCustomerStyle';
-import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome6';
+import { FontWeight } from '../../../constants/theme';
 import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 import { uploadImage } from '../ShipperHTTP';
 import { toast } from '@baronha/ting';
-import { PermissionsAndroid } from 'react-native';
-import { Color, FontFamily, FontWeight, Size } from '../../../constants/theme'
 import ImageChat from './ImageChat';
 import LoadImage from './LoadImage';
 
+const { width, height } = Dimensions.get('window');
 function convertToHHMM(isoString) {
+
     const date = new Date(isoString);
 
     const hours = date.getUTCHours().toString().padStart(2, '0');
@@ -26,27 +26,46 @@ function convertToHHMM(isoString) {
 
     return `${hours}:${minutes}`;
 }
-const renderContentMessage = (type_mess, message) => {
-    switch (type_mess) {
-        case "text":
-            return <Text style={styles.txtMessage}>{message}</Text>
-        case "image":
-            return <Image style={styles.imgMessage} source={{ uri: message }} />
-        default:
-            return null
-    }
-}
 
 const ChatWithCustomer = () => {
     const navigation = useNavigation();
     const route = useRoute();
     const flatListRef = useRef(null);
+    const [scrollPosition, setScrollPosition] = useState(0);
     const { order } = route.params;
-    // console.log(order);
-    // console.log(order);
     const [dataMessage, setdataMessage] = useState(null)
     const { user, sendMessageChat, receiveMessageChat } = useContext(UserContext);
     const [message, setmessage] = useState("")
+    const [isShowImgFull, setisShowImgFull] = useState(false)
+    const [uriFull, seturiFull] = useState(null)
+
+    const scrollToEnd = () => {
+        if (flatListRef.current && dataMessage && dataMessage.length > 0) {
+            flatListRef.current.scrollToEnd({ animated: true });
+        }
+    }
+    useEffect(() => {
+        const isShowImg = async () => {
+            if (dataMessage) {
+                for (let i = 0; i < (dataMessage.length - 1); i++) {
+                    if (dataMessage[i].typeUser == "shipper") {
+                        if (dataMessage[i + 1].typeUser == "shipper") {
+                            dataMessage[i].continue = true
+                        } else {
+                            dataMessage[i].continue = false
+                        }
+                    }
+                }
+            }
+        };
+        isShowImg();
+        setTimeout(() => {
+            scrollToEnd()
+            setTimeout(() => {
+                scrollToEnd()
+            }, 3000);
+        }, 300);
+    }, [dataMessage]);
 
     const takePhoto = useCallback(async response => {
 
@@ -60,6 +79,14 @@ const ChatWithCustomer = () => {
             return;
         }
         if (response.assets && response.assets.length > 0) {
+            const loadingMessage = {
+                type_mess: 'loadingImg',
+                message: '',
+                typeUser: 'shipper',
+                timestamp: new Date().toISOString(),
+            };
+            setdataMessage(prevMessages => [...prevMessages, loadingMessage])
+            
             const asset = response.assets[0];
             const formData = new FormData();
             formData.append('file', {
@@ -69,6 +96,7 @@ const ChatWithCustomer = () => {
             });
             try {
                 const result = await uploadImage(formData);
+                
                 if (result) {
                     handleSendMessage(result.url, "image")
                 } else {
@@ -111,6 +139,7 @@ const ChatWithCustomer = () => {
             saveToPhotos: true,
         };
         launchCamera(options, takePhoto);
+
     }, [takePhoto]);
 
     //hiển thị thư viện
@@ -124,7 +153,7 @@ const ChatWithCustomer = () => {
     }, []);
 
     const handleSendMessage = (message, typeMessage) => {
-        sendMessageChat('shipper', 'chat', order.order, message, typeMessage);
+        sendMessageChat('shipper', 'chat', order, message, typeMessage);
     };
 
     useEffect(() => {
@@ -137,14 +166,12 @@ const ChatWithCustomer = () => {
         };
         if (dataMessage === null)
             fetchData();
-        if (user && receiveMessageChat) {
+        if (flatListRef && user && receiveMessageChat) {
             receiveMessageChat(async message => {
-                console.log(message);
                 switch (message.command) {
                     case 'chat':
-                        if (message.order.orderID == order._id) {
+                        if (message.order && message.order.orderID == order._id) {
                             setdataMessage(message.order.fullChat)
-                            flatListRef.current.scrollToEnd({ animated: true });
                         }
                         break;
                     default:
@@ -156,117 +183,212 @@ const ChatWithCustomer = () => {
             console.log('User is not set or receiveMessage is not defined');
         }
     }, []);
-
+    const renderContentMessage = (type_mess, message, typeUser, timestamp) => {
+        switch (type_mess) {
+            case "text":
+                return (
+                    <View style={[styles.viewMessage, typeUser == 'customer' ? { backgroundColor: '#005987' } : { backgroundColor: '#D3D3D3' }, { paddingVertical: 7 }]}>
+                        <Text style={[styles.txtMessage, typeUser == 'customer' ? { color: 'white' } : { color: '#005987' }]}>{message}</Text>
+                        <Text style={[styles.txtTimeMessage, typeUser == 'customer' ? { color: 'white' } : { color: '#005987' }]}>{convertToHHMM(timestamp)}</Text>
+                    </View>)
+            case "image":
+                return <TouchableOpacity
+                    onPress={() => {
+                        seturiFull(message)
+                        setisShowImgFull(true)
+                    }}
+                >
+                    <ImageChat uri={message} style={styles.imgMessage} scrollToEnd={scrollToEnd} />
+                </TouchableOpacity>
+            case "loadingImg":
+                return (
+                    <View style={[styles.viewMessage, { backgroundColor: '#D3D3D3', paddingVertical: 7 }]}>
+                        <Text style={[styles.txtMessage, { color: '#005987' }]}>Đang gửi ảnh...</Text>
+                    </View>
+                );
+            default:
+                return null
+        }
+    }
+    
     const renderMessage = ({ item }) => {
-
         return (
-            <View style={[item.typeUser == 'shipper' ? { justifyContent: 'flex-end' } : { justifyContent: 'flex-start' }, styles.viewContent]}>
+            <View
+                style={[item.typeUser == 'shipper' ? { justifyContent: 'flex-end' } : { justifyContent: 'flex-start' },
+                item.typeUser == "timeStart" ? { justifyContent: 'center' } : null,
+                styles.viewContent]}
+            >
                 {
-                    (item.typeUser == 'shipper') ? null : <Image style={styles.icAvatarCustomer} source={{ uri: order.shipperID.avatar ? order.shipperID.avatar : 'https://i.pinimg.com/originals/ff/a0/9a/ffa09aec412db3f54deadf1b3781de2a.png' }} />
+                    (item.typeUser == 'customer' && !item.continue) ? <Image style={styles.icAvatarCustomer} source={{ uri: 'https://i.pinimg.com/originals/ff/a0/9a/ffa09aec412db3f54deadf1b3781de2a.png' }} /> : <View style={styles.icAvatarCustomer}></View>
                 }
-                <View style={styles.content}>
-                    {
-                        renderContentMessage(item.type_mess, item.message)
-                    }
-                    {
-                        item.type_mess == "loading" ? null : <Text style={styles.txtTimeMessage}>{convertToHHMM(item.timestamp)}</Text>
-                    }
-
-                </View>
-
+                {
+                    (item.type_mess == "timeStart") ?
+                        <Text style={styles.txtTimeStart}>{convertToHHMM(item.timestamp)}</Text> :
+                        <View style={styles.content}>
+                            {
+                                renderContentMessage(item.type_mess, item.message, item.typeUser, item.timestamp)
+                            }
+                        </View>
+                }
             </View>
         );
     };
+    const handleScroll = (event) => {
+        const { contentOffset, contentSize, layoutMeasurement } = event.nativeEvent;
+        const isEndReached =
+            layoutMeasurement.height + contentOffset.y >= contentSize.height - 20;
+
+        // Xác định hướng lướt
+        const currentPosition = contentOffset.y;
+        const scrollingUp = currentPosition < scrollPosition;
+        const scrollingDown = currentPosition > scrollPosition;
+        setScrollPosition(currentPosition);
+
+        // if (scrollingUp) {
+        //     console.log('Lướt lên');
+        //     // Xử lý sự kiện lướt lên
+        // } else if (scrollingDown) {
+        //     console.log('Lướt xuống');
+        //     // Xử lý sự kiện lướt xuống
+        // }
+
+        if (scrollToEnd && isEndReached && scrollingDown) {
+            setTimeout(() => {
+                if (flatListRef.current && dataMessage && dataMessage.length > 0) {
+                    flatListRef.current.scrollToEnd({ animated: true });
+                }
+            }, 100);
+        }
+    };
     return (
-        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-            <View style={styles.container}>
-                <View style={styles.viewBack}>
-                    <View style={styles.leftBar}>
-                        <TouchableOpacity style={styles.viewICBack}
-                            onPress={() => {
-                                navigation.goBack()
-                            }}
-                        >
-                            <Icon
-                                name="chevron-left"
-                                size={12}
-                                color="#005987"
-                                FontWeight={FontWeight.FW700}
-                            />
-                        </TouchableOpacity>
-                        <LoadImage style={styles.avatarHeader} uri = {order.customerID.avatar} />
-                        <View>
-                            <Text style={styles.nameHeader}>{order.customerID.fullName}</Text>
-                            <Text style={styles.type}>Khách hàng</Text>
-                        </View>
-                    </View>
-                    <TouchableOpacity style={styles.viewICMore}
-                        onPress={() => {
-                            navigation.goBack()
-                        }}
+        <View style={styles.container}>
+            <View style={styles.viewBack}>
+                <View style={styles.leftBar}>
+                    <TouchableOpacity style={styles.viewICBack}
+                        onPress={() => { navigation.goBack() }}
                     >
                         <Icon
-                            name="ellipsis-vertical"
+                            name="chevron-left"
                             size={12}
                             color="#005987"
                             FontWeight={FontWeight.FW700}
                         />
                     </TouchableOpacity>
+                    <Image style={styles.avatarHeader} source={{ uri: "https://inkythuatso.com/uploads/thumbnails/800/2023/03/1-hinh-anh-ngay-moi-hanh-phuc-sieu-cute-inkythuatso-09-13-35-50.jpg" }} />
+                    <View>
+                        <Text style={styles.nameHeader}>Đoàn Thanh Hòa</Text>
+                        <Text style={styles.type}>Khách hàng</Text>
+                    </View>
                 </View>
-                <FlatList style={styles.containerMessage}
-                    data={dataMessage}
-                    renderItem={renderMessage}
-                    keyExtractor={(item, index) => index.toString()}
-                    showsHorizontalScrollIndicator={false}
-                    showsVerticalScrollIndicator={false}
-                    ref={flatListRef}
-                />
-                <View style={styles.viewInput}>
-                    <TouchableOpacity
-                        onPress={openCamera}
-                        style={styles.viewIcImg}>
-                        <Icon
-                            name="camera"
-                            size={22}
-                            color="#005987"
-                        />
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        onPress={openLibrary}
-                        style={styles.viewIcImg}>
-                        <Icon
-                            name="image"
-                            size={22}
-                            color="#005987"
-                        />
-                    </TouchableOpacity>
-                    <TextInput
-                        style={styles.input}
-                        value={message}
-                        onChangeText={(text) => {
-                            setmessage(text)
-                        }}
-                        keyboardType="default"
-                        placeholder=""
+                <TouchableOpacity style={styles.viewICMore}
+                    onPress={() => { navigation.goBack() }}
+                >
+                    <Icon
+                        name="ellipsis-vertical"
+                        size={24}
+                        color="#005987"
+                        FontWeight={FontWeight.FW700}
                     />
-                    <TouchableOpacity
-                        onPress={() => {
-                            handleSendMessage(message, "text")
-                            setmessage('');
-                        }}
-                        style={styles.btnSend}
-                    >
-                        <View>
-                            <Icon
-                                name="paper-plane"
-                                size={22}
-                                color="#005987"
-                            />
-                        </View>
-                    </TouchableOpacity>
-                </View>
+                </TouchableOpacity>
             </View>
-        </TouchableWithoutFeedback>
+            <FlatList style={styles.containerMessage}
+                data={dataMessage}
+                renderItem={renderMessage}
+                keyExtractor={(item, index) => index.toString()}
+                showsHorizontalScrollIndicator={false}
+                showsVerticalScrollIndicator={false}
+                ref={flatListRef}
+                onScroll={handleScroll}
+            >
+            </FlatList>
+
+
+            <View style={styles.viewInput}>
+                <TouchableOpacity
+                    onPress={openCamera}
+                    style={styles.viewIcImg}>
+                    <Icon
+                        name="camera"
+                        size={22}
+                        color="#646982"
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => {
+                        openLibrary()
+                    }}
+                    style={styles.viewIcImg}>
+                    <Icon
+                        name="images"
+                        size={24}
+                        color="#646982"
+                    />
+                </TouchableOpacity>
+                <TextInput
+                    style={styles.input}
+                    value={message}
+                    onChangeText={(text) => setmessage(text)}
+                    keyboardType="default"
+                    placeholder="nhập tin nhắn"
+                    onFocus={() => {
+                        setTimeout(() => {
+                            scrollToEnd()
+                            setTimeout(() => {
+                                scrollToEnd()
+                            }, 300);
+                        }, 300);
+
+                    }}
+
+
+                />
+                <TouchableOpacity
+                    onPress={() => {
+                        handleSendMessage(message, "text")
+                        setmessage("")
+                    }}
+                    style={styles.btnSend}
+                >
+                    <View>
+                        <Icon
+                            name="paper-plane"
+                            size={22}
+                            color="#005987"
+                        />
+                    </View>
+                </TouchableOpacity>
+                <Modal
+                    animationType="fade"
+                    transparent={true}
+                    visible={isShowImgFull}
+                    onRequestClose={setisShowImgFull}
+                >
+                    <View style={{ width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.8)' }}>
+                        <View style={{
+                            width: 50, height: 50, position: 'absolute', zIndex: 100,
+                            bottom: 30, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white',
+                            borderRadius: 25, alignSelf: 'center'
+                        }}>
+                            <TouchableOpacity style={styles.viewICBack}
+                                onPress={() => { setisShowImgFull(false) }}
+                            >
+                                <Icon
+                                    name="circle-xmark"
+                                    size={25}
+                                    color="black"
+                                    FontWeight={FontWeight.FW700}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                        <LoadImage
+                            uri={uriFull}
+                            style={{ width: '100%', height: '100%', zIndex: 80 }}
+                        />
+                    </View>
+
+                </Modal>
+            </View>
+        </View>
     )
 }
 
--- src/components/shipper/styles/ChatWithCustomerStyle.js
+++ src/components/shipper/styles/ChatWithCustomerStyle.js
@@ -1,101 +1,134 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
 import React from 'react'
 import { Color, FontFamily, FontWeight, Size } from '../../../constants/theme'
 
+const { width, height } = Dimensions.get('window');
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    fontFamily: FontFamily.poppins
-  },
-  viewBack: {
+    fontFamily: FontFamily.poppins,
+    position: 'relative'
+},
+viewBack: {
     flexDirection: 'row',
     height: 96,
     alignItems: 'center',
-    borderBottomColor : '#005987',
-    borderBottomWidth : 1,
-    justifyContent : 'space-between',
-    paddingHorizontal : 16,
-    backgroundColor : '#F6F8FA'
-  },
-  barLeft : {
-    flexDirection : 'row'
-  },
-  viewICBack: {
+    borderBottomColor: '#005987',
+    borderBottomWidth: 1,
+    justifyContent: 'space-between',
+    backgroundColor: '#F6F8FA'
+},
+leftBar: {
+    flexDirection: 'row'
+},
+viewICBack: {
     width: 50,
     height: 50,
     justifyContent: 'center',
     alignItems: 'center'
-  },
-  textHeader: {
+},
+avatarHeader: {
+    width: 50,
+    height: 50,
+    borderRadius: 25
+},
+nameHeader: {
     color: '#005987',
-    fontSize: 17,
+    fontSize: 20,
+    fontWeight: '700',
+    marginStart: 13
+},
+type: {
+    color: 'black',
+    fontSize: 14,
     fontWeight: '400',
-    marginStart: 16
-  },
-  viewInput: {
+    marginStart: 13
+},
+viewICMore: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+},
+viewInput: {
     width: '100%',
-    height: 62,
-    backgroundColor: '#F0F5FA',
-    borderRadius: 10,
-    marginTop: 16,
-    borderWidth: 1,
-    borderColor: '#005987',
+    height: 74,
+    borderTopWidth: 1,
+    borderColor: '#DFDFDF',
     justifyContent: 'space-between',
     alignItems: 'center',
     flexDirection: 'row',
-    paddingStart: 10,
-    paddingEnd: 10
-  },
-  viewIcImg: {
+    paddingHorizontal: '3%',
+    position: 'absolute',
+    bottom: 0
+},
+viewIcImg: {
     width: '10%',
-    height: '50%',
+    height: 30,
     alignItems: 'center',
-    justifyContent: 'center'
-  },
-  input: {
+    justifyContent: 'center',
+    marginEnd: 2
+},
+input: {
     fontSize: 14,
-    fontWeight: '700',
+    fontWeight: '400',
     color: '#646982',
-    width: '70%',
-    paddingHorizontal: 15
-  },
-  btnSend: {
-    width: '10%',
-    height: '50%',
+    width: '60%',
+    paddingHorizontal: 15,
+    backgroundColor: '#F0F5FA',
+    borderRadius: 8
+},
+btnSend: {
+    width: 50,
+    height: 50,
     alignItems: 'center',
     justifyContent: 'center'
-  },
-  txtSend: {
+},
+txtSend: {
     color: 'black',
     fontWeight: 'bold'
-  },
-  containerMessage: {
-    width: '90%',
-    height: '80%',
-    alignSelf: 'center',
-    paddingBottom : 15
-  },
-  icAvatarCustomer: {
-    width: 20,
-    marginEnd: 5
-  },
-  txtMessage: {
-    color: 'black',
-    fontSize: 18,
+},
+icAvatarCustomer: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    marginEnd: 10,
+    alignSelf: 'flex-end'
+},
+txtMessage: {
+    fontSize: 20,
     fontFamily: FontFamily.poppins,
-  },
-  txtTimeMessage: {
-    color: Color.text,
-    fontSize: 14,
+    fontWeight: '700'
+},
+txtTimeMessage: {
+    fontSize: 10,
     fontWeight: FontWeight.FW400
-  },
-  viewContent: {
+},
+viewContent: {
     width: '100%',
-    flexDirection: 'row'
-  },
-  imgMessage: {
-    width: 200,
-    aspectRatio: 1,
-    resizeMode: "contain"
-  }
+    flexDirection: 'row',
+},
+imgMessage: {
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: '#005987'
+},
+viewMessage: {
+    paddingHorizontal: 13,
+    borderRadius: 10
+},
+content: {
+    marginVertical: 8
+},
+containerMessage: {
+    alignSelf: 'center',
+    paddingHorizontal: 16,
+    marginBottom: 74
+},
+txtTimeStart: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: '#646982',
+    marginStart : -40
+},
 })
